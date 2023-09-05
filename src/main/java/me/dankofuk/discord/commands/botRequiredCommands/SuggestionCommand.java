a5981215a39dfa55cf01b707db031274dc6260ad
DankOfUK <sage123sage.123@gmail.com>
2023-09-05 22:08:39 +0100

    **Updated** Cleaned up the code/config SuggestionCommand

--- src/main/java/me/dankofuk/discord/commands/botRequiredCommands/SuggestionCommand.java
+++ src/main/java/me/dankofuk/discord/commands/botRequiredCommands/SuggestionCommand.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
+import me.dankofuk.Main;
 import me.dankofuk.discord.DiscordBot;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import org.bukkit.Bukkit;
@@ -23,37 +24,12 @@ import org.bukkit.event.Listener;
 
 public class SuggestionCommand extends ListenerAdapter implements CommandExecutor, Listener {
     private DiscordBot discordBot;
-    private String channelId;
-    private String threadId;
-    private String suggestionMessage;
-    private String noPermissionMessage;
-    private String suggestionUsageMessage;
-    private String responseMessage;
-    private long cooldown;
     private Map<UUID, Long> cooldowns = new HashMap<>();
-    private String title;
-    private String description;
-    private String footer;
-    private Color color;
-    private String thumbnailUrl;
     public FileConfiguration config;
 
 
-    public SuggestionCommand(DiscordBot discordBot, String channelId, String threadId, String suggestionMessage,
-                                     String noPermissionMessage, String suggestionUsageMessage, String responseMessage, long cooldown, String title, String description, String footer, Color color, String thumbnailUrl, FileConfiguration config) {
+    public SuggestionCommand(DiscordBot discordBot, FileConfiguration config) {
         this.discordBot = discordBot;
-        this.channelId = channelId;
-        this.threadId = threadId;
-        this.suggestionMessage = suggestionMessage;
-        this.noPermissionMessage = noPermissionMessage;
-        this.suggestionUsageMessage = suggestionUsageMessage;
-        this.responseMessage = responseMessage;
-        this.cooldown = cooldown;
-        this.title = title;
-        this.description = description;
-        this.footer = footer;
-        this.color = color;
-        this.thumbnailUrl = thumbnailUrl;
         this.config = config;
 
         if (!config.getBoolean("bot.enabled")) {
@@ -61,7 +37,22 @@ public class SuggestionCommand extends ListenerAdapter implements CommandExecuto
         }
     }
 
+    public void accessConfigs() {
+        String suggestionWebhookUrl = Main.getInstance().getConfig().getString("suggestion.webhookUrl");
+        String suggestionThumbnail = Main.getInstance().getConfig().getString("suggestion.thumbnail");
+        String channelId = Main.getInstance().getConfig().getString("suggestion.channelId");
+        String suggestionMessage = Main.getInstance().getConfig().getString("suggestion.message");
+        String suggestionUsageMessage = Main.getInstance().getConfig().getString("suggestion.usageMessage");
+        String responseMessage = Main.getInstance().getConfig().getString("suggestion.sentMessage");
+        long cooldown = Main.getInstance().getConfig().getLong("suggestion.cooldown");
+        String title = Main.getInstance().getConfig().getString("suggestion.title");
+        String description = Main.getInstance().getConfig().getString("suggestion.description");
+        Color color = Color.decode(Main.getInstance().getConfig().getString("suggestion.color"));
+        String footer = Main.getInstance().getConfig().getString("suggestion.footer");
+        String thumbnailUrl = Main.getInstance().getConfig().getString("suggestion.thumbnailUrl");
+        String noPermission = Main.getInstance().getConfig().getString("suggestion.noPermissionMessage");
 
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -76,21 +67,22 @@ public class SuggestionCommand extends ListenerAdapter implements CommandExecuto
         Player player = (Player) sender;
 
         if (!player.hasPermission("commandlogger.suggest.use")) {
-            player.sendMessage(ColorUtils.translateColorCodes(noPermissionMessage));
+            player.sendMessage(ColorUtils.translateColorCodes(Main.getInstance().getConfig().getString("suggestion.noPermissionMessage")));
             return true;
         }
 
         if (args.length < 1) {
-            player.sendMessage(ColorUtils.translateColorCodes(suggestionUsageMessage));
+            player.sendMessage(ColorUtils.translateColorCodes(Main.getInstance().getConfig().getString("suggestion.usageMessage")));
             return true;
         }
 
         String suggest = String.join(" ", args);
         long currentTime = System.currentTimeMillis();
         long lastSuggestTime = cooldowns.getOrDefault(player.getUniqueId(), 0L);
-        long timeRemaining = (lastSuggestTime + (cooldown * 1000L) - currentTime) / 1000L;
+        long timeRemaining = (lastSuggestTime + (Main.getInstance().getConfig().getLong("suggestion.cooldown") * 1000L) - currentTime) / 1000L;
 
         if (timeRemaining > 0) {
+
             String cooldownMessage = "&cPlease wait " + timeRemaining + " seconds before submitting another suggestion.";
             player.sendMessage(ColorUtils.translateColorCodes(cooldownMessage));
             return true;
@@ -100,7 +92,7 @@ public class SuggestionCommand extends ListenerAdapter implements CommandExecuto
         cooldowns.put(player.getUniqueId(), System.currentTimeMillis());
 
         // Send a separate message to the player
-        player.sendMessage(ColorUtils.translateColorCodes(responseMessage.replace("%suggestion%", suggest)));
+        player.sendMessage(ColorUtils.translateColorCodes(Main.getInstance().getConfig().getString("suggestion.sentMessage").replace("%suggestion%", suggest)));
 
         return true;
     }
@@ -108,9 +100,9 @@ public class SuggestionCommand extends ListenerAdapter implements CommandExecuto
     private void sendSuggestion(Player player, String suggestion) {
         CompletableFuture.runAsync(() -> {
             try {
-                TextChannel channel = discordBot.getJda().getTextChannelById(channelId);
+                TextChannel channel = discordBot.getJda().getTextChannelById(Main.getInstance().getConfig().getString("suggestion.channelId"));
                 if (channel == null) {
-                    Bukkit.getLogger().warning("[SuggestionCommandListener] Invalid channel ID specified: " + channelId);
+                    Bukkit.getLogger().warning("[SuggestionCommandListener] Invalid channel ID specified: " + Main.getInstance().getConfig().getString("suggestion.channelId"));
                     return;
                 }
 
@@ -118,11 +110,11 @@ public class SuggestionCommand extends ListenerAdapter implements CommandExecuto
                 String playerUUID = player.getUniqueId().toString();
 
                 EmbedBuilder embed = new EmbedBuilder();
-                embed.setTitle(title.replace("%suggestion%", suggestion).replace("%player%", playerName).replace("%player_uuid%", playerUUID));
-                embed.setDescription(description.replace("%suggestion%", suggestion).replace("%player%", playerName).replace("%player_uuid%", playerUUID));
-                embed.setColor(color);
-                embed.setFooter(footer.replace("%suggestion%", suggestion).replace("%player%", playerName).replace("%player_uuid%", playerUUID), null);
-                embed.setThumbnail(thumbnailUrl);
+                embed.setTitle(Main.getInstance().getConfig().getString("suggestion.title").replace("%suggestion%", suggestion).replace("%player%", playerName).replace("%player_uuid%", playerUUID));
+                embed.setDescription(Main.getInstance().getConfig().getString("suggestion.description").replace("%suggestion%", suggestion).replace("%player%", playerName).replace("%player_uuid%", playerUUID));
+                embed.setColor(Color.decode(Main.getInstance().getConfig().getString("suggestion.color")));
+                embed.setFooter(Main.getInstance().getConfig().getString("suggestion.footer").replace("%suggestion%", suggestion).replace("%player%", playerName).replace("%player_uuid%", playerUUID), null);
+                embed.setThumbnail(Main.getInstance().getConfig().getString("suggestion.thumbnailUrl"));
 
                 channel.sendMessageEmbeds(embed.build()).queue(sentMessage -> {
                     sentMessage.addReaction(Emoji.fromUnicode("U+1F44D")).queue(); // thumbs up reaction
@@ -133,24 +125,4 @@ public class SuggestionCommand extends ListenerAdapter implements CommandExecuto
             }
         });
     }
-
-
-
-
-    public void reloadConfigOptions(DiscordBot discordBot, String channelId, String threadId, String suggestionMessage,
-                                    String noPermissionMessage, String suggestionUsageMessage, String responseMessage, long cooldown, String title, String description, String footer, Color color, String thumbnailUrl) {
-        this.discordBot = discordBot;
-        this.channelId = channelId;
-        this.threadId = threadId;
-        this.suggestionMessage = suggestionMessage;
-        this.noPermissionMessage = noPermissionMessage;
-        this.suggestionUsageMessage = suggestionUsageMessage;
-        this.responseMessage = responseMessage;
-        this.cooldown = cooldown;
-        this.title = title;
-        this.description = description;
-        this.footer = footer;
-        this.color = color;
-        this.thumbnailUrl = thumbnailUrl;
-            }
 }
