6538ad3bca127a634f5b332814ec2c6aed107a3f
DankOfUK <sage123sage.123@gmail.com>
2023-09-15 23:44:45 +0100

    **CHANGED** Main class > KushStaffUtils class

--- src/main/java/me/dankofuk/discord/listeners/StartStopLogger.java
+++ src/main/java/me/dankofuk/discord/listeners/StartStopLogger.java
@@ -1,26 +1,25 @@
 package me.dankofuk.discord.listeners;
 
-import me.dankofuk.Main;
+import me.dankofuk.KushStaffUtils;
 import me.dankofuk.discord.DiscordBot;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.bukkit.configuration.file.FileConfiguration;
 
 import java.awt.*;
 
 public class StartStopLogger extends ListenerAdapter {
 
     private DiscordBot discordBot;
-    public Main main;
+    public KushStaffUtils main;
 
     public StartStopLogger(DiscordBot discordBot) {
         this.discordBot = discordBot;
     }
 
     public void accessConfigs() {
-        String serverStatusChannelId = Main.getInstance().getConfig().getString("serverstatus.channelId");
-        boolean enabled = Main.getInstance().getConfig().getBoolean("serverstatus.enabled");
+        String serverStatusChannelId = KushStaffUtils.getInstance().getConfig().getString("serverstatus.channelId");
+        boolean enabled = KushStaffUtils.getInstance().getConfig().getBoolean("serverstatus.enabled");
     }
 
     public void sendStatusUpdateMessage(boolean serverStarted) {
@@ -29,7 +28,7 @@ public class StartStopLogger extends ListenerAdapter {
             embed.setTitle(serverStarted ? "\uD83D\uDFE2 Server has `started`!" : "\uD83D\uDED1 Server has `shutdown`!");
             embed.setFooter(serverStarted ? "Server Log" : "Server Log");
 
-            TextChannel channel = discordBot.getJda().getTextChannelById(Main.getInstance().getConfig().getString("serverstatus.channelId"));
+            TextChannel channel = discordBot.getJda().getTextChannelById(KushStaffUtils.getInstance().getConfig().getString("serverstatus.channelId"));
             if (channel != null) {
                 channel.sendMessageEmbeds(embed.build()).queue();
             }
