6bc9822ed5d885fcc70bb3a779ad0bd0e3f02224
DankOfUK <sage123sage.123@gmail.com>
2023-09-15 23:49:35 +0100

    **CHANGED** Main class > KushStaffUtils class

--- src/main/java/me/dankofuk/commands/BugCommand.java
+++ src/main/java/me/dankofuk/commands/BugCommand.java
@@ -3,7 +3,7 @@ package me.dankofuk.commands;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import me.dankofuk.Main;
+import me.dankofuk.KushStaffUtils;
 import me.dankofuk.utils.ColorUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -25,7 +25,7 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public class BugCommand implements Listener, CommandExecutor {
-    private Main main;
+    private KushStaffUtils main;
     public FileConfiguration config;
     public final Map<UUID, Long> cooldowns = new HashMap<>();
 
@@ -34,16 +34,16 @@ public class BugCommand implements Listener, CommandExecutor {
     }
 
     public void accessConfigs() {
-        String bugWebhookUrl = Main.getInstance().getConfig().getString("bug_report.webhookUrl");
-        String bugThumbnail = Main.getInstance().getConfig().getString("bug_report.thumbnailUrl");
-        long bugCooldown = Main.getInstance().getConfig().getLong("bug_report.cooldown");;
-        String bugUsername = Main.getInstance().getConfig().getString("bug_report.username");;
-        String bugAvatarUrl = Main.getInstance().getConfig().getString("bug_report.avatarUrl");;
-        boolean isBugEnabled = Main.getInstance().getConfig().getBoolean("bug_report.enabled");;
-        String bugMessage = Main.getInstance().getConfig().getString("bug_report.message");;
-        String noBugPermissionMessage = Main.getInstance().getConfig().getString("bug_report.noPermissionMessage");
-        String bugUsageMessage = Main.getInstance().getConfig().getString("bug_report.usageMessage");;
-        String bugResponse = Main.getInstance().getConfig().getString("bug_report.responseMessage");;
+        String bugWebhookUrl = KushStaffUtils.getInstance().getConfig().getString("bug_report.webhookUrl");
+        String bugThumbnail = KushStaffUtils.getInstance().getConfig().getString("bug_report.thumbnailUrl");
+        long bugCooldown = KushStaffUtils.getInstance().getConfig().getLong("bug_report.cooldown");;
+        String bugUsername = KushStaffUtils.getInstance().getConfig().getString("bug_report.username");;
+        String bugAvatarUrl = KushStaffUtils.getInstance().getConfig().getString("bug_report.avatarUrl");;
+        boolean isBugEnabled = KushStaffUtils.getInstance().getConfig().getBoolean("bug_report.enabled");;
+        String bugMessage = KushStaffUtils.getInstance().getConfig().getString("bug_report.message");;
+        String noBugPermissionMessage = KushStaffUtils.getInstance().getConfig().getString("bug_report.noPermissionMessage");
+        String bugUsageMessage = KushStaffUtils.getInstance().getConfig().getString("bug_report.usageMessage");;
+        String bugResponse = KushStaffUtils.getInstance().getConfig().getString("bug_report.responseMessage");;
     }
 
     @Override
@@ -55,24 +55,24 @@ public class BugCommand implements Listener, CommandExecutor {
         Player player = (Player) sender;
 
         if (!player.hasPermission("commandlogger.bug.use")) {
-            player.sendMessage(ColorUtils.translateColorCodes(Main.getInstance().getConfig().getString("bug_report.noPermissionMessage")));
+            player.sendMessage(ColorUtils.translateColorCodes(KushStaffUtils.getInstance().getConfig().getString("bug_report.noPermissionMessage")));
             return true;
         }
 
-        if (!Main.getInstance().getConfig().getBoolean("bug_report.enabled")) {
+        if (!KushStaffUtils.getInstance().getConfig().getBoolean("bug_report.enabled")) {
             player.sendMessage(ColorUtils.translateColorCodes("&cReporting bugs is currently disabled."));
             return true;
         }
 
         if (args.length < 1) {
-            player.sendMessage(ColorUtils.translateColorCodes(Main.getInstance().getConfig().getString("bug_report.usageMessage")));
+            player.sendMessage(ColorUtils.translateColorCodes(KushStaffUtils.getInstance().getConfig().getString("bug_report.usageMessage")));
             return true;
         }
 
         String bugReport = String.join(" ", args);
         long currentTime = System.currentTimeMillis();
         long lastReportTime = cooldowns.getOrDefault(player.getUniqueId(), 0L);
-        long timeRemaining = (lastReportTime + (Main.getInstance().getConfig().getLong("bug_report.cooldown") * 1000L) - currentTime) / 1000L;
+        long timeRemaining = (lastReportTime + (KushStaffUtils.getInstance().getConfig().getLong("bug_report.cooldown") * 1000L) - currentTime) / 1000L;
 
         if (timeRemaining > 0) {
             String cooldownMessage = "&cPlease wait " + timeRemaining + " seconds before submitting another report.";
@@ -83,7 +83,7 @@ public class BugCommand implements Listener, CommandExecutor {
         cooldowns.put(player.getUniqueId(), System.currentTimeMillis());
 
         // Send a separate message to the player
-        player.sendMessage(ColorUtils.translateColorCodes(Main.getInstance().getConfig().getString("bug_report.responseMessage").replace("%reason%", bugReport)));
+        player.sendMessage(ColorUtils.translateColorCodes(KushStaffUtils.getInstance().getConfig().getString("bug_report.responseMessage").replace("%reason%", bugReport)));
 
         return true;
     }
@@ -92,23 +92,23 @@ public class BugCommand implements Listener, CommandExecutor {
     private void sendWebhook(Player player, String bugReport) {
         CompletableFuture.runAsync(() -> {
             try {
-                URL url = new URL(Main.getInstance().getConfig().getString("bug_report.webhookUrl"));
+                URL url = new URL(KushStaffUtils.getInstance().getConfig().getString("bug_report.webhookUrl"));
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("POST");
                 connection.setRequestProperty("Content-Type", "application/json");
                 connection.setRequestProperty("User-Agent", "BugWebhook");
                 connection.setDoOutput(true);
 
-                String description = ColorUtils.translateColorCodes(Main.getInstance().getConfig().getString("bug_report.message").replace("%player%", player.getName()).replace("%bug%", bugReport));
+                String description = ColorUtils.translateColorCodes(KushStaffUtils.getInstance().getConfig().getString("bug_report.message").replace("%player%", player.getName()).replace("%bug%", bugReport));
                 JsonObject json = new JsonObject();
-                json.addProperty("username", Main.getInstance().getConfig().getString("bug_report.username"));
-                json.addProperty("avatar_url", Main.getInstance().getConfig().getString("bug_report.avatarUrl"));
+                json.addProperty("username", KushStaffUtils.getInstance().getConfig().getString("bug_report.username"));
+                json.addProperty("avatar_url", KushStaffUtils.getInstance().getConfig().getString("bug_report.avatarUrl"));
                 JsonObject embed = new JsonObject();
                 embed.addProperty("description", description);
                 embed.addProperty("color", getColorCode("#FF0000"));
                 embed.addProperty("title", "New Bug Report");
                 JsonObject thumbnail = new JsonObject();
-                thumbnail.addProperty("url", Main.getInstance().getConfig().getString("bug_report.thumbnailUrl"));
+                thumbnail.addProperty("url", KushStaffUtils.getInstance().getConfig().getString("bug_report.thumbnailUrl"));
                 embed.add("thumbnail", thumbnail);
                 JsonArray embeds = new JsonArray();
                 embeds.add(embed);
@@ -129,10 +129,10 @@ public class BugCommand implements Listener, CommandExecutor {
                 //    Bukkit.getLogger().warning("[BugWebhook] Bug report sent to the Discord - Response code: " + responseCode + " Response message: " + responseMessage);
                 //}
             } catch (MalformedURLException e) {
-                Bukkit.getLogger().warning("[BugWebhook] Invalid webhook URL specified: " + Main.getInstance().getConfig().getString("bug_report.webhookUrl"));
+                Bukkit.getLogger().warning("[BugWebhook] Invalid webhook URL specified: " + KushStaffUtils.getInstance().getConfig().getString("bug_report.webhookUrl"));
                 e.printStackTrace();
             } catch (ProtocolException e) {
-                Bukkit.getLogger().warning("[BugWebhook] Invalid protocol specified in webhook URL: " + Main.getInstance().getConfig().getString("bug_report.webhookUrl"));
+                Bukkit.getLogger().warning("[BugWebhook] Invalid protocol specified in webhook URL: " + KushStaffUtils.getInstance().getConfig().getString("bug_report.webhookUrl"));
                 e.printStackTrace();
             } catch (IOException e) {
                 Bukkit.getLogger().warning("[BugWebhook] Error sending message to Discord webhook.");
