8f563a3a7576d9be269389c347efe17a6d7d3830
DankOfUK <sage123sage.123@gmail.com>
2023-09-16 01:06:32 +0100

    **ADDED** Avatar command for DiscordBot

---
+++ src/main/java/me/dankofuk/discord/commands/AvatarCommand.java
@@ -0,0 +1,39 @@
+package me.dankofuk.discord.commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class AvatarCommand extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        if (event.getName().equals("avatar")) {
+            OptionMapping userOption = event.getOption("user");
+
+            if (userOption == null || userOption.getType() != OptionType.USER) {
+                event.reply("Please specify a valid user.").setEphemeral(true).queue();
+                return;
+            }
+
+            String username = Objects.requireNonNull(userOption.getAsUser()).getAsTag();
+            String avatarUrl = Objects.requireNonNull(userOption.getAsUser()).getEffectiveAvatarUrl();
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setColor(Color.BLUE);
+            embed.setTitle("Avatar for " + username);
+            embed.setImage(avatarUrl);
+            embed.setFooter("Avatar Command - BETA");
+
+            event.replyEmbeds(embed.build()).queue();
+        }
+    }
+}
+
