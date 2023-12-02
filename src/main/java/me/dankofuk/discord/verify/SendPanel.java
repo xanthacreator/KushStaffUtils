42e7d8e4f49cf9c17787da5a7c9e2c90e58f2d58
DankOfUK <sage123sage.123@gmail.com>
2023-12-02 21:31:09 +0000

    **ADDED** made configurable

--- src/main/java/me/dankofuk/discord/verify/SendPanel.java
+++ src/main/java/me/dankofuk/discord/verify/SendPanel.java
@@ -13,6 +13,8 @@ import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.bukkit.configuration.Configuration;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class SendPanel extends ListenerAdapter {
 
     public String roleID;
@@ -28,13 +30,18 @@ public class SendPanel extends ListenerAdapter {
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         event.deferReply().queue();
         if (event.getName().equals("sendverifypanel")) {
+            Configuration config = KushStaffUtils.getInstance().getConfig();
             String channelId = event.getOption("channel").getAsString();
             MessageChannel channel = jda.getChannelById(MessageChannel.class, channelId);
+            String sentMessage = config.getString("verify_panel.sentMessage");
+            String buttonMessage = config.getString("verify_panel.buttonMessage");
+            List<String> embedMessageList = config.getStringList("verify_panel.embedMessage");
+            String embedMessage = String.join("\n", embedMessageList);
             if (channel != null) {
-                channel.sendMessage("Click the button to verify!").setActionRow(
-                        Button.primary("verify_button", "Verify")
+                channel.sendMessage(embedMessage).setActionRow(
+                        Button.primary("verify_button", buttonMessage)
                 ).queue();
-                event.reply("Verification panel sent!").setEphemeral(true).queue();
+                event.reply(sentMessage).setEphemeral(true).queue();
             } else {
                 event.reply("Invalid channel ID").setEphemeral(true).queue();
             }
@@ -47,11 +54,11 @@ public class SendPanel extends ListenerAdapter {
             Guild guild = event.getGuild();
             if (guild != null) {
                 Configuration config = KushStaffUtils.getInstance().getConfig();
-                String roleId = config.getString("bot.verify_panel.giveRoleOnClicks");
+                String roleId = config.getString("verify_panel.giveRoleOnClicks");
                 Role role = guild.getRoleById(roleId);
                 if (role != null) {
                     guild.addRoleToMember(UserSnowflake.fromId(event.getUser().getId()), role).queue();
-                    event.reply("You have been verified and assigned the role!").setEphemeral(true).queue();
+                      event.reply(config.getString("verify_panel.roleGivenMessage")).setEphemeral(true).queue();
                 } else {
                     event.reply("Role not found.").setEphemeral(true).queue();
                 }
